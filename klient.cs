0b57f0c1487922214345d1be5aa1f624b3b381d9
katrine911 <kate.efremova@gmail.com>
2017-05-31 19:57:08 +0300

    Update klient.cs

--- klient.cs
+++ klient.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
-
 using ZeroMQ;
-
 namespace Examples
 {
     static partial class Program
@@ -14,31 +12,21 @@ namespace Examples
         {
             if (args == null || args.Length < 1)
             {
-                Console.WriteLine("    Клиент подключен к адресу");
-                Console.WriteLine("               tcp://192.168.0.102:5555");
+                Console.WriteLine("Клиент подключен к адресу");
+                Console.WriteLine("tcp://192.168.0.102:5555");
                 Console.WriteLine();
                 args = new string[] { "tcp://192.168.0.102:5555" };
             }
-
             string endpoint = args[0];
-
-            // Create
             using (var context = new ZContext())
             using (var requester = new ZSocket(context, ZSocketType.REQ))
             {
-                // Connect
                 requester.Connect(endpoint);
-
-
                 Console.WriteLine("Отправь сообщение: \n");
                 string requestText = Console.ReadLine();
                 Console.WriteLine("Sending {0}…", requestText);
                 Console.WriteLine();
-
-                // Send
                 requester.Send(new ZFrame(requestText));
-
-                // Receive
                 using (ZFrame reply = requester.ReceiveFrame())
                 {
                     Console.WriteLine(" Вы отправили сообщение: {0}, Длина сообщения: {1}\n", requestText, reply.ReadString());
