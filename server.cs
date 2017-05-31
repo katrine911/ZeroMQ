10afb3e9cad46636cdc129722aa08dcb6b11ed5a
katrine911 <kate.efremova@gmail.com>
2017-05-31 19:55:48 +0300

    Add files via upload

---
+++ server.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+
+using ZeroMQ;
+
+namespace Examples
+{
+    static partial class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args == null || args.Length < 1)
+            {
+                Console.WriteLine("Привет, добро пожаловать на сервер.");
+                Console.WriteLine();
+            }
+
+            // Create
+            using (var context = new ZContext())
+            using (var responder = new ZSocket(context, ZSocketType.REP))
+            {
+                // Bind
+                responder.Bind("tcp://*:5555");
+
+                while (true)
+                {
+                    // Receive
+                    using (ZFrame request = responder.ReceiveFrame())
+                    {
+                        string receivedString = request.ReadString();
+                        Console.WriteLine("Received {0}", receivedString);
+
+                        // Do some work
+                        Thread.Sleep(1);
+
+                        // Send
+                        string a = Encoding.UTF8.GetBytes(receivedString).Length.ToString();
+                        responder.Send(new ZFrame(a));
+                    }
+                }
+            }
+        }
+    }
+}
