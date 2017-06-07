5c15dc56f75395d6fd3f82dbfd1851cb6bcec6a4
katrine911 <kate.efremova@gmail.com>
2017-06-07 23:11:40 +0300

    Update server.cs

--- server.cs
+++ server.cs
@@ -11,24 +11,24 @@ namespace Examples
     {
         public static void Main(string[] args)
         {
-            if (args == null || args.Length < 1)
+            if (args == null || args.Length < 1) 
             {
                 Console.WriteLine("Добро пожаловать на сервер.");
                 Console.WriteLine();
             }
-            using (var context = new ZContext())
-            using (var responder = new ZSocket(context, ZSocketType.REP))
+            using (var context = new ZContext()) 
+            using (var responder = new ZSocket(context, ZSocketType.REP)) //создем сокеты которые будут принимать сообщения и отсылать ответы
             {
                 responder.Bind("tcp://*:5555");
                 while (true)
                 {
-                    using (ZFrame request = responder.ReceiveFrame())
+                    using (ZFrame request = responder.ReceiveFrame()) //получаем строку с сообщением клиента
                     {
-                        string receivedString = request.ReadString();
-                        Console.WriteLine("Received {0}", receivedString);
+                        string receivedString = request.ReadString(); //получаем консоль
+                        Console.WriteLine("Received {0}", receivedString);  //печатаем консоль
                         Thread.Sleep(1);
-                        string a = Encoding.UTF8.GetBytes(receivedString).Length.ToString();
-                        responder.Send(new ZFrame(a));
+                        string a = Encoding.UTF8.GetBytes(receivedString).Length.ToString(); //Переводим строку в массив байт, получаем его длину и снова переводим в строку
+                        responder.Send(new ZFrame(a)); //отсылаем ответ клиенту
                     }
                 }
             }
